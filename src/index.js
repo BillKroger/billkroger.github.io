a3be9a4f30ff55d89ba0bfeb89f55ce4adf65b25
Bill Kroger <bill.kroger@gmail.com>
2019-08-06 18:40:31 -0700

    set up ReactJS and render test component

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import WorkContainer from './js/containers/WorkContainer.js';
+
+ReactDOM.render(<WorkContainer />, document.getElementById("section2"));
---
+++ src/js/containers/WorkContainer.js
@@ -0,0 +1,20 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class WorkContainer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {};
+    }
+
+    render() {
+        return (
+            <div>Hi</div>
+        );
+    }
+
+}
+
+export default WorkContainer;
