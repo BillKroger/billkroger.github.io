e99e22a608681afc3e9148c1e7515e8a68b98146
Bill Kroger <bill.kroger@gmail.com>
2019-08-31 18:47:10 -0700

    revert publicPath back to /dist/

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
       path: path.resolve(__dirname, "dist/"),
-      publicPath: "/assets/js/",
+      publicPath: "/dist/",
       filename: "main.js"
   },
   devServer: {
